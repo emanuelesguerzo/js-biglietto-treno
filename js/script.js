476988c5c0aab8b324a4dcc756e3a326208d9fe3
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-10-29 11:43:09 +0100

    feat: classroom refactor

--- js/script.js
+++ js/script.js
@@ -2,22 +2,34 @@
 let travelDistance = prompt("Inserisci il numero di chilometri che vuoi percorrere");
 let userAge = prompt("Inserisci la tua età");
 let kmPrice = 0.21;
+let underageDiscount = 20;
+let seniorDiscount = 40;
+const userAgeNum = parseInt(userAge);
+const distance = parseInt(travelDistance);
 
-//ESECUZIONE LOGICA
-let userAgeNum = parseInt(userAge);
-let distance = parseInt(travelDistance);
-let basePrice = distance * kmPrice;
-let finalPrice;
-
-if (isNaN(distance) || isNaN(userAgeNum) || distance <= 0 || userAgeNum <= 0) {
+//CONTROLLO UTENTE
+if (isNaN(distance) || isNaN(userAgeNum) || (distance === null) || (userAgeNum === null) || distance <= 0 || userAgeNum <= 0) {
     console.log("Errore: Assicurati di inserire numeri positivi! Ricarica la pagina!");
-} else if (userAgeNum >= 65) {
-    finalPrice = (basePrice / 100) * 60;
-} else if (userAgeNum < 18) {
-    finalPrice = (basePrice / 100) * 80;
 } else {
-    finalPrice = basePrice;
+
+    //ESECUZIONE LOGICA
+    let basePrice = distance * kmPrice;
+    let discountPercent;
+
+    if (userAgeNum < 18) {
+        discountPercent = underageDiscount;
+    } else if (userAgeNum >= 65) {
+        discountPercent = seniorDiscount;
+    } else {
+        discountPercent = 0;
+    }
+
+    const discountValue = basePrice / 100 * discountPercent;
+    const finalPrice = basePrice - discountValue;
+
+    //OUTPUT
+    const message = `Abbiamo applicato lo sconto del ${discountPercent}% - ${discountValue.toFixed(2)}Euro. Quindi il tuo prezzo finale e' di ${finalPrice.toFixed(2)}Euro`
+    console.log(message);
 }
 
-//OUTPUT
-console.log(`Prezzo del biglietto: ${finalPrice.toFixed(2)}€`);
+
